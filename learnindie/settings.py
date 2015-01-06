1d130aad419972b29c1fd85053051d92df54c30b
jeffyPong <jefferson.zou@yale.edu>
2015-01-06 13:51:51 -0600

    Fix Static Files aka Bootstrap

--- learnindie/settings.py
+++ learnindie/settings.py
@@ -98,6 +98,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Where to go after login
 
